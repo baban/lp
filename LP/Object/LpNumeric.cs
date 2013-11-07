a2c71a2b7ed2c6603ae2817e2e49cdd1f20b194a
BABAN <babanba.n@gmail.com>
2013-11-08 00:17:51 +0900

    Numeric

---
+++ LP/Object/LpNumeric.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP.Object
+{
+    class LpNumeric : LpObject
+    {
+        public static LpObject initialize()
+        {
+            return init(0);
+        }
+
+        public static LpObject initialize(double i)
+        {
+            return init(i);
+        }
+
+        public static LpObject initialize(int i)
+        {
+            return init((double)i);
+        }
+
+        private static LpObject init(double i)
+        {
+            LpObject obj = LpObject.initialize();
+            obj.superclass = LpObject.initialize();
+            obj.doubleValue = i;
+            return obj;
+        }
+
+        protected static LpObject display(LpObject self, LpObject args)
+        {
+            Console.WriteLine(self.doubleValue.ToString());
+            return null;
+        }
+
+        protected static LpObject to_s(LpObject self, LpObject args)
+        {
+            return LpString.initialize(self.doubleValue.ToString());
+        }
+
+        protected static LpObject inspect(LpObject self, LpObject args)
+        {
+            return LpString.initialize(self.doubleValue.ToString());
+        }
+    }
+}
