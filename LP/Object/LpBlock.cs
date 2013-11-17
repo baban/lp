1029dd1d2c687e519b74191953445bdc25cbbea7
BABAN <babanba.n@gmail.com>
2013-11-17 22:30:25 +0900

    block 作成

---
+++ LP/Object/LpBlock.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sprache;
+
+namespace LP.Object
+{
+    class LpBlock : LpObject
+    {
+        public static LpObject initialize()
+        {
+            return init();
+        }
+
+        public static LpObject initialize(string s)
+        {
+            LpObject iobj = init();
+            iobj.statements.Add(s);
+
+            return iobj;
+        }
+
+        static LpObject init()
+        {
+            LpObject iobj = new LpObject();
+            iobj.superclass = new LpObject();
+            iobj.methods["to_s"] = new BinMethod(to_s);
+            iobj.methods["display"] = new BinMethod(display);
+            iobj.methods["execute"] = new BinMethod(execute);
+            iobj.statements = new List<string>();
+
+            return iobj;
+        }
+
+        static LpObject to_s(LpObject self, LpObject args)
+        {
+            self.stringValue = self.ToString();
+            return self;
+        }
+
+        static LpObject display(LpObject self, LpObject args)
+        {
+            var so = to_s(self, args);
+            Console.WriteLine(so.stringValue);
+            return null;
+        }
+
+        static LpObject execute(LpObject self, LpObject args)
+        {
+            LpObject ret = null;
+            self.statements.ForEach(delegate(string stmt)
+            {
+                ret = LpParser.PRIMARY.Parse(stmt);
+            });
+            return ret;
+        }
+    }
+}
---
+++ LpTest/Object/LpBlockTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace LpTest.Object
+{
+    class LpBlockTest
+    {
+        private Type initModule()
+        {
+            Assembly asm = Assembly.LoadFrom("LP.exe");
+            Module mod = asm.GetModule("LP.exe");
+            Type t = mod.GetType("LP.Object.LpObject");
+            return t;
+        }
+
+        private Type initBlockModule()
+        {
+            Assembly asm = Assembly.LoadFrom("LP.exe");
+            Module mod = asm.GetModule("LP.exe");
+            Type t = mod.GetType("LP.Object.LpBlock");
+            return t;
+        }
+
+        [Test]
+        public void initialize()
+        {
+            Type t = initBlockModule();
+            var o = t.GetMethod("initialize", BindingFlags.Static | BindingFlags.Public, null, Type.EmptyTypes, null).Invoke(null, null);
+            Assert.AreEqual("LP.Object.LpObject", o.GetType().ToString());
+        }
+
+        [Test]
+        public void initialize1()
+        {
+            Type t = initBlockModule();
+            var o = t.GetMethod("initialize", BindingFlags.Static | BindingFlags.Public, null, new Type[]{ typeof(string) }, null).Invoke(null, new object[]{ (string)"10" });
+            Assert.AreEqual("LP.Object.LpObject", o.GetType().ToString());
+        }
+
+        [Test]
+        public void to_s()
+        {
+            Type t = initBlockModule();
+            var o = t.GetMethod("initialize", BindingFlags.Static | BindingFlags.Public, null, Type.EmptyTypes, null).Invoke(null, null);
+
+            // 引数なし
+            var prms = new object[] { o, o };
+            var so = t.GetMethod("to_s", BindingFlags.Static | BindingFlags.NonPublic).Invoke(null, prms);
+            var str = so.GetType().InvokeMember("stringValue", BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetField, null, so, null);
+            var r = new Regex(@"<obj \w+?>");
+            Assert.True(r.IsMatch(str.ToString()));
+        }
+
+        [Test]
+        public void display()
+        {
+            Type t = initBlockModule();
+            var o = t.GetMethod("initialize", BindingFlags.Static | BindingFlags.Public, null, Type.EmptyTypes, null).Invoke(null, null);
+
+            // 引数なし
+            var prms = new object[] { o, o };
+            var so = t.GetMethod("display", BindingFlags.Static | BindingFlags.NonPublic).Invoke(null, prms);
+        }
+
+        [Test]
+        public void execute()
+        {
+            Type t = initBlockModule();
+            var o = t.GetMethod("initialize", BindingFlags.Static | BindingFlags.Public, null, Type.EmptyTypes, null).Invoke(null, null);
+
+            // 引数なし
+            var prms = new object[] { o, o };
+            var so = t.GetMethod("execute", BindingFlags.Static | BindingFlags.NonPublic).Invoke(null, prms);
+        }
+
+        [Test]
+        public void execute1()
+        {
+            Type t = initBlockModule();
+            var o = t.GetMethod("initialize", BindingFlags.Static | BindingFlags.Public, null, new Type[] { typeof(string) }, null).Invoke(null, new object[] { (string)"10" });
+
+            // 引数なし
+            var prms = new object[] { o, o };
+            var so = t.GetMethod("execute", BindingFlags.Static | BindingFlags.NonPublic).Invoke(null, prms);
+            Assert.AreEqual("LP.Object.LpObject", so.GetType().ToString());
+            Assert.AreEqual(10.0, so.GetType().InvokeMember("doubleValue", BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetField, null, so, null));
+        }
+    
+    }
+}
