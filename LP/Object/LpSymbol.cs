20a6b9fb3c26aa659d7a6c217bdb83aae9265f87
BABAN <babanba.n@gmail.com>
2014-01-02 09:25:18 +0900

    make symbol methods

--- LP/Object/LpSymbol.cs
+++ LP/Object/LpSymbol.cs
@@ -6,12 +6,64 @@ using System.Threading.Tasks;
 
 namespace LP.Object
 {
-    // TODO: ==
-    // TODO: ===
-    // TODO: display
-    // TODO: inspect
-    // TODO: to_s
-    class LpSymbol
+    class LpSymbol : LpObject
     {
+        public static LpObject initialize(string s)
+        {
+            return init(s);
+        }
+
+        private static LpObject init(string s)
+        {
+            LpObject obj = LpObject.initialize();
+            setMethods(obj);
+            obj.superclass = LpObject.initialize();
+            obj.stringValue = s;
+            obj.class_name = "symbol";
+            return obj;
+        }
+
+        private static void setMethods(LpObject obj)
+        {
+            // TODO: ==
+            // TODO: ===
+            // TODO: display
+            obj.methods["inspect"] = new BinMethod(inspect);
+            obj.methods["to_s"] = new BinMethod(to_s);
+            obj.methods["display"] = new BinMethod(display);
+
+            //obj.methods["="] = new BinMethod(setOp);
+            obj.methods["=="] = new BinMethod(equal);
+            obj.methods["==="] = new BinMethod(eq);
+        }
+
+        protected static LpObject to_s(LpObject self, LpObject args)
+        {
+            return self;
+        }
+
+        protected static LpObject inspect(LpObject self, LpObject args)
+        {
+            return self;
+        }
+
+        protected static LpObject display(LpObject self, LpObject args)
+        {
+            Console.WriteLine(self.stringValue);
+            return null;
+        }
+
+        private static LpObject equal(LpObject self, LpObject args)
+        {
+            var o = args.arrayValues.ElementAt(0);
+            return LpBool.initialize(self.stringValue == o.stringValue);
+        }
+
+        // TODO: This method must comare to memory address!!
+        private static LpObject eq(LpObject self, LpObject args)
+        {
+            var o = args.arrayValues.ElementAt(0);
+            return LpBool.initialize(self.stringValue == o.stringValue);
+        }
     }
 }
