003779bc8520f670ed1416166628a1e60c6a90b4
BABAN <babanba.n@gmail.com>
2014-01-13 21:23:36 +0900

    関数オブジェクト追加

---
+++ LpTest/Object/LpLambdaTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace LpTest.Object
+{
+    [TestFixture]
+    class LpLambdaTest
+    {
+    }
+}
