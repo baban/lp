fd45e7b7c9529c135eff09babd8be28b0ea7ff98
baban <babanba.n@gmail.com>
2015-07-21 01:38:22 +0900

    parser re-structure

--- LP/Object/LpArray.cs
+++ LP/Object/LpArray.cs
@@ -19,7 +19,7 @@ namespace LP.Object
         public static LpObject initialize(string[] args)
         {
             var o = init();
-            o.arrayValues = args.Select( (arg) => LpParser.parseStmtObject(arg) ).ToList();
+            o.arrayValues = args.Select( (arg) => Parser.BaseParser.parseStmtObject(arg) ).ToList();
             return o;
         }
 
