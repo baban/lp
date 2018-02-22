23521fdfe4adc2d39cdf3eccfdbec8f465d94f28
masanao.matsubara <m_matsubara@okwave.co.jp>
2018-02-22 23:31:51 +0900

    2項演算子を実装

--- LP/Parser/LpParser.cs
+++ LP/Parser/LpParser.cs
@@ -27,8 +27,43 @@ namespace LP.Parser
             var Hash = new NonTerminal("Hash", typeof(Node.Hash));
             var Primary = new NonTerminal("Primary", typeof(Node.Primary));
 
+            var Expr11 = new NonTerminal("Expr11", typeof(Node.BinExpr));
+            var Expr10 = new NonTerminal("Expr10", typeof(Node.BinExpr));
+            var Expr9 = new NonTerminal("Expr9", typeof(Node.BinExpr));
+            var Expr8 = new NonTerminal("Expr8", typeof(Node.BinExpr));
+            var Expr7 = new NonTerminal("Expr7", typeof(Node.BinExpr));
+            var Expr6 = new NonTerminal("Expr6", typeof(Node.BinExpr));
+            var Expr5 = new NonTerminal("Expr5", typeof(Node.BinExpr));
+            var Expr4 = new NonTerminal("Expr4", typeof(Node.BinExpr));
+            var Expr3 = new NonTerminal("Expr3", typeof(Node.BinExpr));
+            var Expr2 = new NonTerminal("Expr2", typeof(Node.BinExpr));
+            var Expr1 = new NonTerminal("Expr1", typeof(Node.BinExpr));
+            var Expr0 = new NonTerminal("Expr0", typeof(Node.BinExpr));
             var Expr = new NonTerminal("Expr", typeof(Node.Expr));
+
             var Stmt = new NonTerminal("Stmt", typeof(Node.Stmt));
+
+            RegisterBracePair("(", ")");
+            RegisterOperators(0, "=");
+            RegisterOperators(1, "and", "or");
+            RegisterOperators(2, "..", "^..", "..^", "^..^");
+            RegisterOperators(3, "||");
+            RegisterOperators(4, "&&");
+            RegisterOperators(5, "<=>", "===", "==", "!=", "=~", "!~");
+            RegisterOperators(6, ">=", ">", "<=", "<");
+            RegisterOperators(7, "|");
+            RegisterOperators(8, "&");
+            RegisterOperators(9, "<<", ">>");
+            RegisterOperators(10, "+", "-");
+            RegisterOperators(11, "*", "/", "%");
+            RegisterOperators(12, "-");
+            RegisterOperators(13, "**");
+            RegisterOperators(14, "not");
+            RegisterOperators(15, "+", "!", "~");
+            RegisterOperators(16, "++", "--");
+            MarkPunctuation(",", "(", ")");
+            //this.Delimiters = "{}[](),:;+-*/%&|^!~<>=";
+            this.MarkPunctuation(";", ",", "(", ")", "{", "}", "[", "]", ":");
 
             Numeric.Rule = new NumberLiteral("Number");
             Str.Rule = new StringLiteral("String", "\"");
@@ -42,10 +77,33 @@ namespace LP.Parser
             Hash.Rule = ToTerm("{") + Assoc + ToTerm("}");
             Primary.Rule = Numeric | Str | Bool | Nl | Symbol | Array | Hash;
 
-            Expr.Rule = Primary;
+            Expr11.Rule = makeChainOperators(new string[] { "*", "/", "%" }, Expr);
+            Expr10.Rule = makeChainOperators(new string[] { "+", "-" }, Expr);
+            Expr9.Rule = makeChainOperators(new string[] { "<<", ">>" }, Expr);
+            Expr8.Rule = makeChainOperators(new string[] { "&" }, Expr);
+            Expr7.Rule = makeChainOperators(new string[] { "|" }, Expr);
+            Expr6.Rule = makeChainOperators(new string[] { ">=", ">", "<=", "<" }, Expr);
+            Expr5.Rule = makeChainOperators(new string[] { "<=>", "===", "==", "!=", "=~", "!~" }, Expr);
+            Expr4.Rule = makeChainOperators(new string[] { "&&" }, Expr);
+            Expr3.Rule = makeChainOperators(new string[] { "||" }, Expr);
+            Expr2.Rule = makeChainOperators(new string[] { "..", "^..", "..^", "^..^" }, Expr);
+            Expr1.Rule = makeChainOperators(new string[] { "and", "or" }, Expr);
+            Expr0.Rule = makeChainOperators(new string[] { "=" }, Expr);
+            Expr.Rule = Expr11 | Expr10 | Expr9 | Expr8 | Expr7 | Expr6 | Expr5 | Expr4 | Expr3 | Expr2 | Expr1 | Primary;
+
             Stmt.Rule = Expr;
 
             Root = Stmt;
+        }
+
+        BnfExpression makeChainOperators(string[] operands, NonTerminal beforeExpr)
+        {
+            return operands.Select((op) => makeChainOperator(op, beforeExpr)).Aggregate((a, b) => a | b);
+        }
+
+        BnfExpression makeChainOperator(string op, NonTerminal beforeExpr)
+        {
+            return beforeExpr + ToTerm(op) + beforeExpr;
         }
     }
 }
