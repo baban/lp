534def6c917ce55b45068c205e336e2c1e7a0ddd
masanao.matsubara <m_matsubara@okwave.co.jp>
2018-03-18 00:38:34 +0900

    コメントせいり

--- LP/Node/QuestionQuote.cs
+++ LP/Node/QuestionQuote.cs
@@ -13,20 +13,14 @@ namespace LP.Node
         {
             base.Init(context, treeNode);
             var node = treeNode.GetMappedChildNodes().Last();
-            System.Console.WriteLine("QuasiQuote");
             Node = AddChild("QuotedVariable", node);
         }
 
         protected override object DoEvaluate(ScriptThread thread)
         {
             thread.CurrentNode = this;
-            System.Console.WriteLine("Node");
-            System.Console.WriteLine(Node);
-            var var = (Object.LpObject)Node.Evaluate(thread);
-            System.Console.WriteLine("aaaaaa");
-            System.Console.WriteLine( var.class_name);
-            System.Console.WriteLine(var.statements);
-            var result = (Object.LpObject)var.statements.Evaluate(thread);
+            var quote = (Object.LpObject)Node.Evaluate(thread);
+            var result = (Object.LpObject)quote.statements.Evaluate(thread);
             thread.CurrentNode = Parent;
 
             return result;
