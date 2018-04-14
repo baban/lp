7a515bccbc34199246a8656d7c5db88f8228ce2a
baban <babanba.n@gmail.com>
2018-04-14 18:34:04 +0900

    refctring parser

---
+++ LP/Node/Through.cs
@@ -0,0 +1,26 @@
+﻿using Irony.Ast;
+using Irony.Interpreter;
+using Irony.Interpreter.Ast;
+using Irony.Parsing;
+
+namespace LP.Node
+{
+    public class Through : AstNode
+    {
+        public AstNode Node { get; private set; }
+        public override void Init(AstContext context, ParseTreeNode treeNode)
+        {
+            base.Init(context, treeNode);
+            var nodes = treeNode.GetMappedChildNodes();
+            Node = AddChild("Node", nodes[0]);
+        }
+
+        protected override object DoEvaluate(ScriptThread thread)
+        {
+            thread.CurrentNode = this;
+            Object.LpObject result = (Object.LpObject)Node.Evaluate(thread);
+            thread.CurrentNode = Parent;
+            return result;
+        }
+    }
+}
